71b711c288a656bc26dbb4897608f4d0f921cfa5
79tanka <72666478+79tanka@users.noreply.github.com>
2022-05-10 21:28:49 +0300

    viewcontroller

---
+++ modules/controller.js
@@ -0,0 +1,43 @@
+export class Controller{
+  constructor(game, view){
+    this.game=game;
+    this.view=view;
+  }
+  init(keyCode){
+    window.addEventListener('keydown', event=>{
+      if (event.code===keyCode){
+        this.view.init();
+        this.start();
+      }
+    })
+  }
+  start(){
+    this.view.showArea(this.game.viewArea);
+    setInterval(()=>{
+      this.game.moveDown();
+      this.view.showArea(this.game.viewArea);
+    },1000);
+    window.addEventListener('keydown', event => {
+      const key=event.code;
+  switch (key) {
+  case 'ArrowDown':
+    this.game.moveDown();
+    this.view.showArea(this.game.viewArea);
+    break;
+  case 'ArrowUp':
+    this.game.rotateTetromino();
+    this.view.showArea(this.game.viewArea);
+    break;
+  case 'ArrowLeft':
+    this. game.moveLeft();
+    this.view.showArea(this.game.viewArea);
+    break;
+  case 'ArrowRight':
+    this.game.moveRight();
+    this.view.showArea(this.game.viewArea);
+    break;
+  }
+  });
+  }
+    
+}
